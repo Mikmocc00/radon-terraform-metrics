6c77c71a36981e79bbe8de749e346b228c668ab0
Alessandro De Cicco <alessandro@MacBook-Air-di-Alessandro-2.local>
2026-04-14 12:47:30 +0200

    New

--- radon_terraform_metrics/complex_metrics/avg_block_verbosity.py
+++ radon_terraform_metrics/complex_metrics/avg_block_verbosity.py
@@ -1,30 +1,21 @@
-import math
-from radon_terraform_metrics.general.text_entropy import TextEntropy
-from radon_terraform_metrics.general.num_tokens import NumTokens
+import re
 
-
-class VocabularyRichness:
+class AvgBlockVerbosity:
     """
-    Misura la diversità lessicale del file: quanto il vocabolario
-    è ricco e non ripetitivo rispetto all'entropia massima possibile.
-    Formula: text_entropy / log2(num_tokens)
-    Valore in [0, 1]: vicino a 1 = vocabolario ricco e vario.
+    Verbosità media per risorsa:
+    numero di token / numero di risorse.
     """
 
     def __init__(self, script):
         self.script = script
 
-    def compute(self):
+    def count(self):
 
-        entropy = TextEntropy(self.script).count()
-        tokens = NumTokens(self.script).count()
+        tokens = len(re.findall(r'\S+', self.script))
 
-        if tokens < 2:
+        resources = len(re.findall(r'^\s*resource\s+"', self.script, re.MULTILINE))
+
+        if resources == 0:
             return 0.0
 
-        max_entropy = math.log2(tokens)
-
-        if max_entropy == 0:
-            return 0.0
-
-        return entropy / max_entropy
+        return tokens / resources
