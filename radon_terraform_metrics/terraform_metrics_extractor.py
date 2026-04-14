941c9511674fd5dedae1216f99684cf05e568c76
Alessandro De Cicco <alessandro@MacBook-Air-di-Alessandro-2.local>
2026-04-14 12:06:21 +0200

    Solved Problem

--- radon_terraform_metrics/terraform_metrics_extractor.py
+++ radon_terraform_metrics/terraform_metrics_extractor.py
@@ -6,10 +6,10 @@ def extract_all(script: str):
     if script is None:
         raise TypeError('Expected a string')
 
-    metrics = general_metrics
+    metrics = {}
+    metrics.update(general_metrics)
     metrics.update(configuration_metrics)
     metrics.update(complex_metrics)
-
     results = dict()
 
     for name in metrics:
